c66aaff03d2854184e88bdb9b8de8d57cb0da6f1
Diyorbek Burxonov <diyorburxonov0007@gmail.com>
2024-10-09 18:48:04 +0500

    update

--- src/components/ui/category-card.tsx
+++ src/components/ui/category-card.tsx
@@ -5,10 +5,10 @@ interface Props {
 
 const CategoryCard = ({ title, img }: Props) => {
   return (
-    <div className="bg-[#f7f8f7] flex-1 flex justify-center items-center flex-col min-w-[200px] py-6 cursor-pointer">
+    <div className="bg-[#f7f8f7] flex-1 flex justify-center items-center flex-col min-w-[200px] py-6 cursor-pointer md:min-w-[100px]">
       <img className=" mb-14 h-[125px]" src={img} />
       <div className="flex-col justify-start items-center gap-4 inline-flex">
-        <div className="text-[#221a25] text-lg font-black font-['Ruda'] leading-snug">
+        <div className="text-[#221a25] text-lg font-black font-['Ruda'] leading-snug md:text-sm">
           {title}
         </div>
         <div className="hover:bg-[#2cdf70]/10 w-11 h-11 flex justify-center items-center transition duration-300">
